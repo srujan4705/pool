6ae322aa4e05a5234f46a00766c008fc51f493f9
srujan4705 <22951a67b5@iare.ac.in>
2025-04-02 22:00:50 +0530

    day 3
    almost donrfrontend

--- app.py
+++ app.py
@@ -4,7 +4,8 @@ app = Flask(__name__)
 
 # Home Page Route
 @app.route('/')
-def home():
+@app.route('/index')
+def index():
     return render_template('index.html')
 
 # Survey Page Route
@@ -16,6 +17,29 @@ def survey():
 @app.route('/therapies')
 def therapies():
     return render_template('therapiesPage.html')
+
+# Products Page Route
+@app.route('/products')
+def products():
+    return render_template('productPage.html')
+
+# Add this route for the cryotherapy page
+@app.route('/therapies/cryotherapy')
+def therapy_cryotherapy():
+    return render_template('therapy_cryotherapy.html')
+@app.route('/book-appointment')
+def booking():
+    return render_template('booking.html')
+@app.route('/about')
+def about():
+    return render_template('about.html')
+    # ... existing imports and code ...
+
+@app.route('/faq')
+def faq():
+    return render_template('faq.html')
+
+# ... rest of your code ...
 
 if __name__ == '__main__':
     app.run(debug=True)
